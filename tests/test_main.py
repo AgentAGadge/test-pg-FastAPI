ea2f3343069c7e23f1c4b5757d5fd0be06afac2e
Mathieu Lamiot <mathieu.lamiot@free.fr>
2023-04-03 21:09:01 +0200

    Additional tests for "addone"

--- tests/test_main.py
+++ tests/test_main.py
@@ -18,8 +18,41 @@ from src.main import app
     (1.2, "tp_package", 2.2),
 ])
 @pytest.mark.anyio
-async def test_addone(addend, method, expected):
+async def test_addone_success(addend, method, expected):
     async with AsyncClient(app=app, base_url="http://test/") as ac:
         response = await ac.get("/addone/"+str(addend)+"?method="+str(method))
     assert response.status_code == 200
     assert response.json() == {"sum": expected}
+
+
+@pytest.mark.parametrize("addend, method, expected", [
+    (0, "unknown", 1),
+    ("string", "python", 1),
+    (complex(1, 1), "python", complex(2, 1))
+])
+@pytest.mark.anyio
+async def test_addone_wrong_param(addend, method, expected):
+    async with AsyncClient(app=app, base_url="http://test/") as ac:
+        response = await ac.get("/addone/"+str(addend)+"?method="+str(method))
+    assert response.status_code == 422
+
+
+@pytest.mark.parametrize("addend, method, expected", [
+    (0, "python", 1),
+])
+@pytest.mark.anyio
+async def test_addone_optional_param(addend, method, expected):
+    async with AsyncClient(app=app, base_url="http://test/") as ac:
+        response = await ac.get("/addone/"+str(addend))
+    assert response.status_code == 200
+    assert response.json() == {"sum": expected}
+
+
+@pytest.mark.parametrize("addend, method, expected", [
+    (0, "python", 1),
+])
+@pytest.mark.anyio
+async def test_addone_missing_param(addend, method, expected):
+    async with AsyncClient(app=app, base_url="http://test/") as ac:
+        response = await ac.get("/addone/?method=python")
+    assert response.status_code == 404
