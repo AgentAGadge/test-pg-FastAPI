82422177dc4b6cf895ac806f56d242622818af2f
Mathieu Lamiot <mathieu.lamiot@free.fr>
2023-04-10 17:35:40 +0200

    As a codebase owner, I want the linter to check docstrings Fixes #7
     - Add pylint (req, CI, config)
     - Fix all pylint errors/warnings

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,1 +1,3 @@
 flake8 .
+pylint src
+pylint tests
--- tests/integration/test_addition.py
+++ tests/integration/test_addition.py
@@ -24,21 +24,29 @@ TEST_ADDITION_ADDITION_URL = "/addition/"
 ])
 @pytest.mark.anyio
 async def test_addone_success(addend, method, expected):
-    async with AsyncClient(app=app, base_url=TEST_ADDITION_BASE_URL) as ac:
-        response = await ac.get(TEST_ADDITION_ADDITION_URL+"addone/"+str(addend)+"?method="+str(method))
+    """
+        Checks success and result of addone call in various configurations.
+    """
+    async with AsyncClient(app=app, base_url=TEST_ADDITION_BASE_URL) as async_controller:
+        response = await async_controller.get(
+            TEST_ADDITION_ADDITION_URL+"addone/"+str(addend)+"?method="+str(method))
     assert response.status_code == 200
     assert response.json() == {"sum": expected}
 
 
-@pytest.mark.parametrize("addend, method, expected", [
-    (0, "unknown", 1),
-    ("string", "python", 1),
-    (complex(1, 1), "python", complex(2, 1))
+@pytest.mark.parametrize("addend, method", [
+    (0, "unknown"),
+    ("string", "python"),
+    (complex(1, 1), "python")
 ])
 @pytest.mark.anyio
-async def test_addone_wrong_param(addend, method, expected):
-    async with AsyncClient(app=app, base_url=TEST_ADDITION_BASE_URL) as ac:
-        response = await ac.get(TEST_ADDITION_ADDITION_URL+"addone/"+str(addend)+"?method="+str(method))
+async def test_addone_wrong_param(addend, method):
+    """
+        Checks status_code of addone call in case of wrong parameter type or value.
+    """
+    async with AsyncClient(app=app, base_url=TEST_ADDITION_BASE_URL) as async_controller:
+        response = await async_controller.get(
+            TEST_ADDITION_ADDITION_URL+"addone/"+str(addend)+"?method="+str(method))
     assert response.status_code == 422
 
 
@@ -47,17 +55,22 @@ async def test_addone_wrong_param(addend, method, expected):
 ])
 @pytest.mark.anyio
 async def test_addone_optional_param(addend, expected):
-    async with AsyncClient(app=app, base_url=TEST_ADDITION_BASE_URL) as ac:
-        response = await ac.get(TEST_ADDITION_ADDITION_URL+"addone/"+str(addend))
+    """
+        Checks success and result of addone call in case of missing optional parameter.
+    """
+    async with AsyncClient(app=app, base_url=TEST_ADDITION_BASE_URL) as async_controller:
+        response = await async_controller.get(
+            TEST_ADDITION_ADDITION_URL+"addone/"+str(addend))
     assert response.status_code == 200
     assert response.json() == {"sum": expected}
 
 
-@pytest.mark.parametrize("addend, method, expected", [
-    (0, "python", 1),
-])
 @pytest.mark.anyio
-async def test_addone_missing_param(addend, method, expected):
-    async with AsyncClient(app=app, base_url=TEST_ADDITION_BASE_URL) as ac:
-        response = await ac.get(TEST_ADDITION_ADDITION_URL+"addone/?method=python")
+async def test_addone_missing_param():
+    """
+        Checks status_code of addone call in case of missing mandatory parameter.
+    """
+    async with AsyncClient(app=app, base_url=TEST_ADDITION_BASE_URL) as async_controller:
+        response = await async_controller.get(
+            TEST_ADDITION_ADDITION_URL+"addone/?method=python")
     assert response.status_code == 404
